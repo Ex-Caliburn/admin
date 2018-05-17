25a204b18143a863cd2c4da892b7074f96c859d7
lijiye <lijiye@jinddaka.com>
2018-05-17 11:40:29 +0800

    kyls

---
+++ src/views/audit/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-container">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'audit',
+  directives: {
+  },
+  data() {
+    return {
+      activeName: 'directly',
+      inputData: 'https://github.com/PanJiaChen/vue-element-admin'
+    }
+  },
+  methods: {
+    handleCopy(text, event) {
+    },
+    clipboardSuccess() {
+      this.$message({
+        message: '复制成功',
+        type: 'success',
+        duration: 1500
+      })
+    }
+  }
+}
+</script>
+
