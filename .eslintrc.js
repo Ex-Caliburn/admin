b15c2593d040a37cc26945a3eb2b7309be71bded
lijiye <764291507@qq.com>
2018-05-30 02:50:35 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,8 +27,8 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    'no-unused-vars': "ignore",
-    'no-console ': "ignore",
+    'no-unused-vars': 0,
+    'no-console ': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // 命名的函数忽略括号前的空格检测
